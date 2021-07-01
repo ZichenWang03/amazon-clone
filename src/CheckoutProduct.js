39817e3ae19624b424e37e7bd9bacfd9b97651b7
Zichen Wang <zichenwang7633@gmail.com>
2021-07-01 16:48:28 -0400

    updated checkout page

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -1,12 +1,20 @@
 import React from "react";
 import "./CheckoutProduct.css";
+import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct({ id, image, title, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+  const removeFromBasket = () => {
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
   return (
     <div className="checkoutProduct">
       <img className="checkoutProduct__image" src={image} />
 
-      < className="checkoutProduct__info">
+      <div className="checkoutProduct__info">
         <p className="checkoutProduct__title">{title}</p>
         <p className="checkoutProduct__price">
           <small>$</small>
@@ -19,7 +27,7 @@ function CheckoutProduct({ id, image, title, price, rating }) {
               <p>🌟</p>
             ))}
         </div>
-        <button></button>
+        <button onClick={removeFromBasket}>Remove from Basket</button>
       </div>
     </div>
   );
